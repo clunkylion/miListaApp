8646265012d310bdfd142fe0b4c6ba73a3192eeb
José Cortés <cote99salamanca@gmail.com>
2020-10-11 00:13:54 -0300

    se creo modal para crear cuaderno segun color

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from "react-native";
+import colors from "./src/styles/Colors";
+import { AntDesign } from "@expo/vector-icons";
+import tempData from "./src/utils/temporalData";
+import TodoList from "./src/components/TodoList";
+import AddListModal from "./src/components/AddListModal";
 export default function App() {
+  const [visibleTodo, setVisibleTodo] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal
+        animationType="slide"
+        visible={visibleTodo}
+        onRequestClose={visibleTodo}
+      >
+        <AddListModal closeModal={() => setVisibleTodo(false)} />
+      </Modal>
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>
+          Mi{" "}
+          <Text style={{ fontWeight: "300", color: colors.blue }}>Lista</Text>
+        </Text>
+        <View style={styles.divider} />
+      </View>
+      <View style={{ marginVertical: 48 }}>
+        <TouchableOpacity
+          style={styles.addList}
+          onPress={() => setVisibleTodo(true)}
+        >
+          <AntDesign name="plus" size={16} color={colors.blue} />
+        </TouchableOpacity>
+        <Text style={styles.add}>Agregar</Text>
+      </View>
+      <View style={{ height: 275, paddingLeft: 32 }}>
+        <FlatList
+          data={tempData}
+          keyExtractor={(item) => item.name}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <TodoList list={item} />}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +57,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   },
 });
